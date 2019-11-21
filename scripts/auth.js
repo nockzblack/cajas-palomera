3848027c2688950dcc4dd84b594348619d67846c
Fer Benavides <nockzblack@gmail.com>
2019-11-20 19:16:38 -0600

    add log in option
    Signed-off-by: Fer Benavides <nockzblack@gmail.com>

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,30 +1,52 @@
+// listen for auth status changes
+auth.onAuthStateChanged(user => {
+     
+})
+
 // sign up
 const signUpForm = document.querySelector('#signUpForm');
 signUpForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     // get user info
-    const email = signUpForm['email'].value;
-    const password = signUpForm['password'].value;
+    const email = signUpForm['email-register'].value;
+    const password = signUpForm['password-register'].value;
     console.log(email,password);
 
     // sign up the user
-    
     auth.createUserWithEmailAndPassword(email,password).then(cred => {
         console.log(cred);
-        const modal = document.querySelector("#modalSignUp");
-        //modal.modal('hide');
-        $('#modalSignUp').modal('hide');
+        // closing modal
+        $('#signUpModal').modal('hide');
         signUpForm.reset();
     })
-
-    const logOut = document.querySelector('#logOut');
-
-    logOut.addEventListener('click', (e) => {
-        e.preventDefault();
-        auth.signOut().then( () => {
-            console.log('user signedOut');
-        })
-    })
-    
 });
+
+//log Out
+const logOut = document.querySelector('#logOut');
+
+logOut.addEventListener('click', (e) => {
+    e.preventDefault();
+    auth.signOut().then( () => {
+        console.log('user signedOut');
+    })
+})
+
+
+// login
+const loginForm = document.querySelector('#logInForm');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    // get user info
+    const email = loginForm['email-logIn'].value;
+    const password = loginForm['password-logIn'].value;
+    console.log(email,password);
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
+        // closing modal
+        $('#logInModal').modal('hide');
+        loginForm.reset();
+    })
+})
